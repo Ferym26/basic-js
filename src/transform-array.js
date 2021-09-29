bf5f759bf8bbdc850ddecb03a73c3ce53fe334b4
ferym26 <ferym26@gmail.com>
2021-09-29 17:01:49 +0300

    feat: done transform array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+	NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,7 +15,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+	if (!(arr instanceof Array)) {
+		throw new Error(`'arr' parameter must be an instance of the Array!`)
+	}
+	const arrResult = [];
+	const arro = [...arr];
+	for (let i = 0; i < arro.length; i++) {
+		if (arro[i] == '--discard-next') {
+			arro[i + 1] = '--false';
+		} else if (arro[i] == '--discard-prev') {
+			if (arro[i - 1] != '--false' && arro[i - 1] !== undefined) arrResult.pop();
+		} else if (arro[i] == '--double-next') {
+			if (arro[i + 1] != undefined) arrResult.push(arro[i + 1]);
+		} else if (arro[i] == '--double-prev') {
+			if (arro[i - 1] != undefined && arro[i - 1] != '--false') arrResult.push(arro[i - 1]);
+		} else {
+			if (arro[i] != '--false') arrResult.push(arro[i]);
+		}
+	}
+	return arrResult;
 }
