08fbc1342f75d69e8ca3d1d670d1ee8622a8184c
ferym26 <ferym26@gmail.com>
2021-09-29 16:15:03 +0300

    feat: done extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,4 +1,6 @@
-import { NotImplementedError } from '../extensions/index.js';
+import {
+	NotImplementedError
+} from '../extensions/index.js';
 
 /**
  * Create a repeating string based on the given parameters
@@ -15,7 +17,24 @@ import { NotImplementedError } from '../extensions/index.js';
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-export default function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+export default function repeater(str, {
+	repeatTimes = 1,
+	separator = '+',
+	addition,
+	additionRepeatTimes = 1,
+	additionSeparator = '|'
+}) {
+	let result = str;
+	if (str === null) str = 'null';
+	if (addition === null) addition = 'null';
+	if (addition != undefined) {
+		let k1 = [];
+		for (let i = 0; i < additionRepeatTimes; i++) k1.push(addition);
+		k1 = k1.join(additionSeparator);
+		result = str + k1;
+	}
+	let k2 = [];
+	for (let i = 0; i < repeatTimes; i++) k2.push(result);
+	k2 = k2.join(separator);
+	return k2;
+};
